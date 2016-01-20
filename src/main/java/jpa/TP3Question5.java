ae0425838e506c4f95f8d8be0c434263b1b916b0
pitou35 <pierreaugustin.totaro03@gmail.com>
2016-01-20 15:55:44 +0100

    test question 5

--- src/main/java/jpa/TP3Question5.java
+++ src/main/java/jpa/TP3Question5.java
@@ -1,8 +1,65 @@
 package jpa;
 
-public class TP3Question5 {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.Person;
+
+@WebServlet(name="addHouse",urlPatterns={"/addHouse"})
+public class TP3Question5 extends HttpServlet{
 
 	public static void main(String[] args) {
 		
+		
+	}
+	
+	public void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+		throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+			EntityManagerFactory factory = Persistence
+					.createEntityManagerFactory("mysql");
+			EntityManager manager = factory.createEntityManager();
+			EntityTransaction tx = manager.getTransaction();
+			tx.begin();
+			
+			try {
+				Person p1=new Person(request.getParameter("nom"), request.getParameter("prenom"), request.getParameter("mail"));
+				manager.persist(p1);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			tx.commit();
+			manager.close();
+			factory.close();
+			
+			
+			
+			out.println("<HTML>\n<BODY>\n" +
+					"<H1>Recapitulatif des informations</H1>\n" +
+					"<UL>\n" +			
+			" <LI>Nom: "
+					+ request.getParameter("email") + "\n" +
+					" <LI>Prenom: "
+					+ request.getParameter("nom") + "\n" +
+					" <LI>Age: "
+					+ request.getParameter("prenom") + "\n" +
+					"</UL>\n" +				
+			"</BODY></HTML>");
 	}
 }
