49d9188dd9d54b79279f04ee142ce8fecbc6484e
PAT <pierreaugustin.totaro03@gmail.com>
2016-02-19 22:02:27 +0100

    redacton rapport: correction question 7

--- src/main/java/rest/SampleWebService.java
+++ src/main/java/rest/SampleWebService.java
@@ -1,12 +1,19 @@
 package rest;
 
+import java.util.Collection;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import domain.Heater;
 import domain.Home;
+import domain.Person;
+
+
 
 @Path("/hello")
 public class SampleWebService {
@@ -22,13 +29,24 @@ public class SampleWebService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Home getHome() {
 	    Home h = new Home(1,1);
-	    //h.setName("toto");
+	    
 	    Heater h1 = new Heater(2000);
-	    //h1.setPower("500w");
+	    
 	    Heater h2 = new Heater(3000);
-	    //h2.setPower("600w");
+	    
 	    h.addChauffages(h1);
 	    h.addChauffages(h2);
 	    return h;
 	}
+	
+    /*
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/home")
+	public Collection<Home> getHomes() {
+		String query = "select h from Home as h";
+		List result = EntityManagerHelper.getEntityManager().createQuery(query).getResultList();
+		return result;
+	}*/
+	
 }
